294ebf81f795a01bbd81b9817b7c156987e18430
79dric <audriclorenzogomezcarrera@gmail.com>
2026-09-14 21:54:34 -0600

    Update activity.py

--- activity.py
+++ activity.py
@@ -7,7 +7,7 @@ Temperatura= st.number_input("Introduce la temperatura=",
 
 
 
-if Temperatura.value <=10:
+if Temperatura<=10:
   print("Hace frio")
 elif 10<=Temperatura<=24:
   print("La temperatura es agradable")
