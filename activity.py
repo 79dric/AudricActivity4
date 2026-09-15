834fc3dffc93d14276bdd4bcfc6df3d10d625ea5
79dric <audriclorenzogomezcarrera@gmail.com>
2026-09-14 21:47:56 -0600

    Update activity.py

--- activity.py
+++ activity.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
 st.title("Clasificador de temperatura")
-Temperatura= st.number_input()
+Temperatura= st.number_input("Introduce la temperatura=",
+                            value=20
+)
+if Temperatura <=10:
+  print("Hace frio")
+elif 10<=Temperatura<=24:
+  print("La temperatura es agradable")
+else:
+  print("hace calor")
